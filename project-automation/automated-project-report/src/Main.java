a326e6b2b514119f1061e797abdd6abfdd38f7a9
Janik <janik.scholl@accu-rate.de>
2018-07-25 14:21:53 +0200

    Set-up template code for project report automation

---
+++ project-automation/automated-project-report/src/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
+public class Main {
+
+    static final Path baseFolder = Paths.get(System.getProperty("user.dir"));
+
+    public static void main(String[] args) throws IOException {
+        OverwriteTemplates.writeMainPage();
+        OverwriteTemplates.writeIntroduction();
+
+        buildPdf();
+    }
+
+    private static void buildPdf() throws IOException {
+        ProcessBuilder pb = new ProcessBuilder();
+        // To use a bibliography the process is: latex -> bibtex -> latex -> latex
+        pb.directory(baseFolder.toFile());
+        pb.command("pdflatex", "main-page.tex");
+        consumeOutput(pb.start());
+        consumeOutput(pb.start()); // run it twice for the TOC to be built with correct links
+
+        // clean up temporary tex-build-files
+        for (File file : Objects.requireNonNull(baseFolder.toFile().listFiles())) {
+            if (file.getName().endsWith(".aux") || file.getName().endsWith(".toc")
+                    || file.getName().endsWith(".log") || file.getName().endsWith(".blg")) {
+                file.delete();
+            }
+        }
+    }
+
+
+    private static void consumeOutput(Process proc) throws IOException { // via stackoverflow.com/a/5711150
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+        BufferedReader stdError = new BufferedReader(new InputStreamReader(proc.getErrorStream()));
+        String s;
+        while ((s = stdInput.readLine()) != null) {
+            System.out.println(s);
+        }
+        while ((s = stdError.readLine()) != null) {
+            System.err.println(s);
+        }
+    }
+}
---
+++ project-automation/automated-project-report/src/OverwriteTemplates.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class OverwriteTemplates {
+
+    static void writeIntroduction() throws IOException {
+        Path template = Main.baseFolder.resolve("templates").resolve("introduction.tex");
+        Path target = Main.baseFolder.resolve("introduction.tex");
+        String content = new String(Files.readAllBytes(template));
+        content = content.replace("<REPLACE_ME>",
+                "I still think Janik is wonderful.");
+        String replacePath = "figures/daumen.png";
+        content = content.replace("<REPLACE_ME_IMAGE>", replacePath);
+
+        Files.write(target, content.getBytes(StandardCharsets.UTF_8));
+    }
+
+    static void writeMainPage() throws IOException {
+        Path template = Main.baseFolder.resolve("templates").resolve("main-page.tex");
+        Path target = Main.baseFolder.resolve("main-page.tex");
+        String content = new String(Files.readAllBytes(template));
+        Files.write(target, content.getBytes(StandardCharsets.UTF_8));
+    }
+}
